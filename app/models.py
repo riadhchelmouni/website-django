51563c58c5250306d8e8c9318dc5b358c0cd0b91
riadhchelmouni <chelmouni.yazidriadh@gmail.com>
2024-08-09 10:59:53 +0200

    first commit

---
+++ app/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+STATE_CHOICES=(
+    ('Andaman & Nicobar Islands','Andaman & Nicobar Islands'),
+    ('Andhra Pradesh','Andhra Pradesh'),
+    ('Arunachal Pradesh','Arunachal Pradesh'),
+    ('Assam','Assam'),
+    ('Bihar','Bihar'),
+    ('Chandigarh','Chandigarh'),
+    ('Chhattisgarh','Chhattisgarh'),
+    ('Dadra & Nagar Haveli','Dadra & Nagar Haveli'),
+    ('Daman & Diu','Daman & Diu'),
+    ('Delhi','Delhi'),
+    ('Goa','Goa'),
+    ('Gujarat','Gujarat'),
+    ('Haryana','Haryana'),
+    ('Himachal Pradesh','Himachal Pradesh'),
+    ('Jammu & Kashmir','Jammu & Kashmir'),
+    ('Jharkhand','Jharkhand'),
+    ('Karnataka','Karnataka'),
+    ('Kerala','Kerala'),
+    ('Lakshadweep','Lakshadweep'),
+    ('Madhya Pradesh','Madhya Pradesh'),
+    ('Maharashtra','Maharashtra'),
+    ('Manipur','Manipur'),
+    ('Meghalaya','Meghalaya'),
+    ('Mizoram','Mizoram'),
+    ('Nagaland','Nagaland'),
+    ('Odisha','Odisha'),
+    ('Puducherry','Puducherry'),
+    ('Punjab','Punjab'),
+    ('Rajasthan','Rajasthan'),
+    ('Sikkim','Sikkim'),
+    ('Tamil Nadu','Tamil Nadu'),
+    ('Telangana','Telangana'),
+    ('Tripura','Tripura'),
+    ('Uttar Pradesh','Uttar Pradesh'),
+    ('Uttarakhand','Uttarakhand'),
+    ('West Bengal','West Bengal'),
+)
+
+CATEGORY_CHOICES=(
+    ('CR','Curd'),
+    ('ML','Milk'),
+    ('LS','Lassi'),
+    ('MS','Milkshake'),
+    ('PN','Paneer'),
+    ('GH','Ghee'),
+    ('CZ','Cheese'),
+    ('IC','Ice-Creams'),
+)
+
+
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
+    composition = models.TextField(default= '')
+    prodapp = models.TextField(default= '')
+    #brand =  models.CharField(max_length= 100)
+    category = models.CharField(choices= CATEGORY_CHOICES, max_length= 2)
+    product_image = models.ImageField(upload_to= 'product')
+    def __str__(self):
+        return self.title
+    
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    mobile = models.IntegerField(default=0)
+    zipcode = models.IntegerField()
+    state = models.CharField(choices=STATE_CHOICES,max_length=100)
+    def __str__(self):
+        return self.name
