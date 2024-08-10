0eb7f9164242a6d03bdf42a06d462f0dee71a47b
riadhchelmouni <chelmouni.yazidriadh@gmail.com>
2024-08-10 21:52:03 +0200

    first commit

---
+++ myproject/myapp/apps.py
@@ -0,0 +1,7 @@
+# myapp/apps.py
+
+from django.apps import AppConfig
+
+class MyappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myapp'
---
+++ myproject/myapp/static/myapp/js/scripts.js
@@ -0,0 +1,2 @@
+// Add any JavaScript functionality here
+console.log("Scripts loaded.");
